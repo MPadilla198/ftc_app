68802dea45e1677b8a7574bf1d56637096b63d5a
Miguel Padilla <miguel.padilla.harmony2013@gmail.com>
2017-01-25 14:36:17 -0600

    Update BasicAutoL.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/BasicAutoL.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/BasicAutoL.java
@@ -5,8 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Miguel Padilla on 11/12/16.
@@ -36,23 +34,23 @@ public class BasicAutoL extends LinearOpMode {
     private ColorSensor whiteLineFinder;
     
     private enum travelDir {
-      NORTH(1, -1, 1, -1),
-      NORTHEAST(1, 0, 0, -1),
-      EAST(-1, -1, 1, 1),
-      SOUTHEAST(0, -1, 1, 0),
-      SOUTH(-1, 1, -1, 1),
-      SOUTHWEST(-1, 0, 0, 1),
-      WEST(1, 1, -1, -1),
-      NORTHWEST(0, 1, -1 , 0);
+      	NORTH(1, -1, 1, -1),
+     	NORTHEAST(1, 0, 0, -1),
+     	EAST(-1, -1, 1, 1),
+      	SOUTHEAST(0, -1, 1, 0),
+      	SOUTH(-1, 1, -1, 1),
+      	SOUTHWEST(-1, 0, 0, 1),
+      	WEST(1, 1, -1, -1),
+      	NORTHWEST(0, 1, -1 , 0);
       
-      public final int brVal, blVal, frVal, flVal;
+      	public final int brVal, blVal, frVal, flVal;
 		
-      travelDir(br, bl, fr, fl) {
-        this.brVal = br;
-        this.blVal = bl;
-        this.frVal = fr;
-        this.flVal = fl;
-      }
+     	travelDir(br, bl, fr, fl) {
+        	this.brVal = br;
+     	   	this.blVal = bl;
+        	this.frVal = fr;
+        	this.flVal = fl;
+      	}
     }
 
     @Override
@@ -64,13 +62,8 @@ public class BasicAutoL extends LinearOpMode {
         backRightMotor = hardwareMap.dcMotor.get("br"); // 1.0 == clock-wise
         frontLeftMotor = hardwareMap.dcMotor.get("fl"); // 1.0 == clock-wise
         frontRightMotor = hardwareMap.dcMotor.get("fr"); // 1.0 == clock-wise
-
-        //backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        //backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        //frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        //frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
-
-
+		
+		// Retrieve sensor ports
         colorSensor1 = hardwareMap.colorSensor.get("cs1");
         colorSensor1.enableLed(false);
         colorSensor2 = hardwareMap.colorSensor.get("cs2");
@@ -79,9 +72,6 @@ public class BasicAutoL extends LinearOpMode {
         colorSensor3.enableLed(false);
         whiteLineFinder = hardwareMap.colorSensor.get("wlf");
         whiteLineFinder.enableLed(true);
-
-        //possibly have to set direction for motors in reverse
-
 
         // Resets encoders, but this cuts power to the motors
         backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -99,9 +89,39 @@ public class BasicAutoL extends LinearOpMode {
         backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
+		
+		////////////////////////////////
+		// Start Program
+		////////////////////////////////
         startWhiteLineTravel();
-
+		
+		// Some way to travel to the wall and stop before it hits; slightly less than 3 inches away
+		
+		
+		
+		// Checks colors and hits blue button
+		if (colorSensor2.blue() > colorSensor2.red() && colorSensor1.blue() > colorSensor1.red()) {
+			hitAndCheckButton1();
+			linearTravel(ticksPerInch * 5.5, 0.2, travelDir.SOUTH);
+		} else {
+			linearTravel(ticksPerInch * 5.5, 0.2, travelDir.SOUTH);
+			hit AndCheckButton2();
+		}
+		
+		// travels close to the beacon then uses the color sensor to get it perfectly on the white line and lined up with the button
+		sinusoidalTravel(ticksPerInches * 42, 1.0, travelDir.SOUTH);
+		secondWhiteLineTravel();
+		
+		if (colorSensor2.blue() > colorSensor2.red() && colorSensor1.blue() > colorSensor1.red()) {
+			hitAndCheckButton1();
+			linearTravel(ticksPerInch * 5.5, 0.2, travelDir.SOUTH);
+		} else {
+			linearTravel(ticksPerInch * 5.5, 0.2, travelDir.SOUTH);
+			hit AndCheckButton2();
+		}
+		
+		linearTravel(ticksPerInch * 72 * Math.sqrt(2), 1.0, travelDir.NORTHWEST);
+		
         stop();
     }
 	
@@ -117,19 +137,64 @@ public class BasicAutoL extends LinearOpMode {
         backLeftMotor.setPower(-0.2);
         frontRightMotor.setPower(0.2);
 		
-		double currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3.0;
-		double lastColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3.0;
+		int currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
+		int lastColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
 
-        while (Math.abs(currentColor - lastColor) < 3) {
+        while (Math.abs(currentColor - lastColor) < 5) {
 			lastColor = currentColor;
-			currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3.0;
+			currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
         }
 		
 		backLeftMotor.setPower(0);
 		frontRightMotor.setPower(0);
 		
-		blEncoderPosition = backLeftMotor.currentPosition(); // Not sure if currentPosition is the right method name for finding current encoder value
-		frEncoderPosition = frontLeftMotor.currentPosition(); 
+		blEncoderPosition = backLeftMotor.getPosition(); // Not sure if currentPosition is the right method name for finding current encoder value
+		frEncoderPosition = frontLeftMotor.getPosition(); 
+	}
+	
+	private void secondWhiteLineTravel() {
+		final double speed = 0.2;
+		final travelDir dir = travelDir.SOUTH;
+		final int dDistance = ticksPerInch * 100; // Arbitrary
+		
+		final int brInitPos = brEncoderPosition;
+      	final int blInitPos = blEncoderPosition;
+      	final int frInitPos = frEncoderPosition;
+      	final int flInitPos = flEncoderPosition;
+      
+      	brEncoderPosition += dir.brVal * dDistance;
+      	blEncoderPosition += dir.blVal * dDistance;
+      	frEncoderPosition += dir.frVal * dDistance;
+      	flEncoderPosition += dir.flVal * dDistance;
+		
+		backRightMotor.setPosition(brEncoderPosition);
+		backLeftMotor.setPosition(blEncoderPosition);
+		frontRightMotor.setPosition(frEncoderPosition);
+		frontLeftMotor.setPosition(flEncoderPosition);
+		
+		backRightMotor.setPower(speed);
+		backLeftMotor.setPower(speed);
+		frontRightMotor.setPower(speed);
+		frontLeftMotor.setPower(speed);
+		
+		int currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
+		int lastColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
+      
+      	while((Math.abs(lastColor - currentColor) < 5) {
+        	// (:		
+			lastColor = currentColor;
+			currentColor = (whiteLineFinder.blue() + whiteLineFinder.red() + whiteLineFinder.green()) / 3;
+		}
+			  
+		backRightMotor.setPower(0);
+		backLeftMotor.setPower(0);
+		frontRightMotor.setPower(0);
+		frontLeftMotor.setPower(0);
+			  
+		brEncoderPosition = backRightMotor.getPosition();
+		blEncoderPosition = backLeftMotor.getPosition();
+		frEncoderPosition = frontRightMotor.getPosition();
+		flEncoderPosition = frontLeftMotor.getPosition();
 	}
     
     private void sinusoidalTravel(int dDistance, double maxSpeed, travelDir dir) {
@@ -152,7 +217,6 @@ public class BasicAutoL extends LinearOpMode {
       
       	do {
         	// (:
-        
         	double brCurrentSpeed = maxSpeed * Math.sin(Math.PI * ((double) (brEncoderPosition - brInitPos) / (double) dDistance + 0.1));
         	double blCurrentSpeed = maxSpeed * Math.sin(Math.PI * ((double) (blEncoderPosition - blInitPos) / (double) dDistance + 0.1));
         	double bfCurrentSpeed = maxSpeed * Math.sin(Math.PI * ((double) (frEncoderPosition - frInitPos) / (double) dDistance + 0.1));
@@ -169,9 +233,7 @@ public class BasicAutoL extends LinearOpMode {
               	frontLeftMotor.isBusy());
     }
 	
-	private void linearTravel(int dDistance, double speed, travelDir dir) {
-		maxSpeed -= 0.1;
-		
+	private void linearTravel(int dDistance, double speed, travelDir dir) {		
       	final int brInitPos = brEncoderPosition;
       	final int blInitPos = blEncoderPosition;
       	final int frInitPos = frEncoderPosition;
@@ -200,18 +262,18 @@ public class BasicAutoL extends LinearOpMode {
               	frontLeftMotor.isBusy());
 	}
 	
-	private void checkOrHitButton1() {
-		while (colorSensor1.blue() > colorSensor1.red() && colorSensor2.blue() > colorSensor2.red()) {
+	private void hitAndCheckButton1() {
+		do {
 			linearTravel(3, 0.25, travelDir.EAST);
 			linearTravel(3, 0.25, travelDir.WEST);
-		}
+		} while (colorSensor1.blue() > colorSensor1.red() && colorSensor2.blue() > colorSensor2.red());
 	}
 	
-	private void checkOrHitButton2() { 
-		while (colorSensor3.blue() > colorSensor3.red() && colorSensor2.blue() > colorSensor2.red()) {
+	private void hitAndCheckButton2() { 
+		do {
 			linearTravel(3, 0.25, travelDir.EAST);
 			linearTravel(3, 0.25, travelDir.WEST);
-		}
+		} while (colorSensor3.blue() > colorSensor3.red() && colorSensor2.blue() > colorSensor2.red());
 	}
 
     private void addAndUpdateData(String caption, Object value) {
