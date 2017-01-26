20520114693a238c226a20d6408aa8661b72f095
Miguel Padilla <miguel.padilla.harmony2013@gmail.com>
2017-01-26 15:45:05 -0600

    Update RangeTester.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RangeTester.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RangeTester.java
@@ -1,49 +1,56 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceReader;
-import org.swerverobotics.library.SynchronousOpMode;
-import org.swerverobotics.library.interfaces.TeleOp;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
 /**
- *This is a test program to see how accurate range sensors are.
+ * Created by Miguel Padilla on 11/12/16.
  */
-@TeleOp(name = "RangeTester")
-public class RangeTester extends SynchronousOpMode {
- 
+
+@Autonomous(name="RangeTester", group="AutoOp")
+public class RangeTester extends LinearOpMode {
+
+    // Sensors
+    private ColorSensor colorSensor2;
+
+    // Setup for range sensor because it's stupid and I hate it......
+    byte[] range1Cache;
+    I2cAddr RANGE1ADDRESS = new I2cAddr(0x14);
+    public static final int RANGE1_REG_START = 0x04;
+    public static final int RANGE1_READ_LENGTH = 2;
+    public I2cDevice RANGE1;
+    public I2cDeviceSynch RANGE1Reader;
+
     @Override
-    public void main() throws InterruptedException {
-        I2cDevice range;
-        range = hardwareMap.i2cDevice.get("range");
-        I2cDeviceReader rangeReader = new I2cDeviceReader(range, 0x14, 0x04, 2);
- 
-        telemetry.addData("readerLength", rangeReader.getReadBuffer().length);
-        telemetry.update();
- 
+    public void runOpMode() throws InterruptedException {
         waitForStart();
- 
-        while (opModeIsActive()) {
-            byte rangeReadings[] = rangeReader.getReadBuffer();
-            telemetry.addData("Uncalibrated: ", rangeReadings[0]);
-            telemetry.addData("Calibrated: ", calibratedUltrasonic(rangeReadings[0]));
+
+        // Retrieve sensor ports
+        colorSensor2 = hardwareMap.colorSensor.get("cs2");
+        colorSensor2.enableLed(false);
+
+        // Initialzing range sensor stuff
+        RANGE1 = hardwareMap.i2cDevice.get("range");
+        RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
+        RANGE1Reader.engage();
+
+        ////////////////////////////////
+        // Start Program
+        ////////////////////////////////
+        while (true) {
+            range1Cache = RANGE1Reader.read(RANGE1_REG_START, RANGE1_READ_LENGTH);
+            telemetry.addData("Red: ", colorSensor2.red());
+            telemetry.addData("Green: ", colorSensor2.green());
+            telemetry.addData("Blue: ", colorSensor2.blue());
+            telemetry.addData("Ultra Sonic: ", range1Cache[0] & 0xFF);
+            telemetry.addData("ODS: ", range1Cache[1] & 0xFF);
             telemetry.update();
-        }
-    }
-    public int calibratedUltrasonic(byte reading) {
-        //convert byte to int
-        int uncalibratedDistance = reading;
-        if (uncalibratedDistance == -1) {
-            return -1;
-        }
- 
-        int calibratedDistance;
- 
-        if (uncalibratedDistance >= 0) {
-            return calibratedDistance = (int) (((double)uncalibratedDistance / 9) * 10);
-        }
-        else {
-            calibratedDistance = uncalibratedDistance * -1;
-            telemetry.addData("after flip: ", calibratedDistance);
-            calibratedDistance = 255 - calibratedDistance;
-            telemetry.addData("after 255-: ", calibratedDistance);
-            return (int) (((double)calibratedDistance / 9) * 10);
         }
     }
 }
